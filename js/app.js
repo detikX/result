7b2268d29d9247481d48f9320c108aa20ff3c2d3
dediwibisono17 <dedi.wibisono17@gmail.com>
2024-02-21 14:40:11 +0700

    ded

--- js/app.js
+++ js/app.js
@@ -233,3 +233,34 @@ $.ajax({
 $(document).ready(function () {
     // $('#myTable').DataTable();
 });
+
+
+
+
+function animateValue(obj, start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+        if (!startTimestamp) startTimestamp = timestamp;
+        const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+        obj.innerHTML = Math.floor(progress * (end - start) + start);
+        if (progress < 1) {
+            window.requestAnimationFrame(step);
+        }
+    };
+    window.requestAnimationFrame(step);
+}
+
+const obj = document.getElementById("val-one");
+animateValue(obj, 0, 25, 5000);
+setTimeout(() => {
+    $("#val-one").text('25,31')
+}, 5500)
+
+const obj1 = document.getElementById("value1");
+animateValue(obj1, 0, 70, 20000);
+
+const obj2 = document.getElementById("value2");
+animateValue(obj2, 0, 23, 20000);
+
+const obj3 = document.getElementById("value3");
+animateValue(obj3, 0, 58, 20000);
